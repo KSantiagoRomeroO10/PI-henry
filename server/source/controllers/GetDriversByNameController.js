3c9bd3cd0b64eb69fc7d1ce8885d51ffcb837e65
Santiago Romero <KevinSRomeroO10@hotmail.com>
2023-10-15 12:43:51 -0500

    Perfeccionando el Get driver by name, ya está perfecto.

--- server/source/controllers/GetDriversByNameController.js
+++ server/source/controllers/GetDriversByNameController.js
@@ -1,33 +1,40 @@
-const { Op } = require('sequelize');
-const Driver = require('../models/Driver');
-const axios = require('axios');
+const { Op } = require('sequelize')
+const Driver = require('../models/Driver')
+const axios = require('axios')
 
 const GetDriversByNameController = async (req, res) => {
   try {
-    const { name } = req.query;
+    const { name } = req.query
     
     const driversFromDB = await Driver.findAll({
       where: {
         nombre: {
-          [Op.iLike]: `%${name}%`,
-        },
-      },
-      limit: 15
-    });
+          [Op.iLike]: `%${name}%`
+        }
+      }
+    })
 
-    const apiResponse = await axios.get(`http://localhost:5000/drivers?name.forename=${name}`);
-    const apiDrivers = apiResponse.data;
+    const apiResponse = await axios.get(`http://localhost:5000/drivers`);
+    const apiDrivers = apiResponse.data;    
 
-    if (driversFromDB.length === 0 && apiDrivers.length === 0) {
-      return res.status(404).json({ mensaje: 'No se encontraron conductores con el nombre especificado.' });
+    const filteredApiDrivers = apiDrivers.filter(driver => driver.name.forename.includes(name))
+
+    if (driversFromDB.length === 0 && filteredApiDrivers.length === 0) {
+      return res.status(404).json({ mensaje: 'No se encontraron conductores con el nombre especificado.' })
     }
+    
+    const mergeDrivers = [...driversFromDB, ...filteredApiDrivers]
 
-    res.status(200).json({ driversFromDB, apiDrivers });
+    const max15 = mergeDrivers.slice(0, 15)
+
+    res.status(200).json({"Drivers": max15})
   } 
   catch (error) {
-    console.error('Error al obtener drivers por nombre:', error);
-    res.status(500).json({ mensaje: 'Error interno del servidor al buscar conductores por nombre.' });
+    res.status(500).json({ 
+      mensaje: 'Error interno del servidor al buscar conductores por nombre.', 
+      error: error.message 
+    })
   }
-};
+}
 
-module.exports = { GetDriversByNameController };
+module.exports = { GetDriversByNameController }
